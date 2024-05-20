a5583d74f44413d1a69909840465acae07c3c9f0
Matan Lurey <matan@lurey.org>
2024-05-20 09:52:24 -0700

    +

--- lib/src/ffi/_libc/_posix.dart
+++ lib/src/ffi/_libc/_posix.dart
@@ -7,7 +7,7 @@ import 'package:dt/src/ffi/libc.dart';
 
 /// Whether the current platform supports the C standard library.
 final isSupported = const [
-  'fflush',
+  // 'fflush',
   'free',
   'malloc',
   'write',
